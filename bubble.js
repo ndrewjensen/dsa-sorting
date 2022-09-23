bcfca1a7f4f1336d60d5a43f73a8482431ce14eb
Andrew Jensen <andrewkjensen@gmail.com>
2022-09-23 11:57:36 -0700

    complete bubble and merge

--- bubble.js
+++ bubble.js
@@ -1,3 +1,17 @@
-function bubbleSort() {}
+"use strict";
+
+/** Given an array, sort the values in the array.
+ * An O(n^2) algorithm.  */
+
+function bubbleSort(nums) {
+  for (let i = nums.length - 1; i > -1; i--) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+      }
+    }
+  }
+  return nums
+}
 
 module.exports = bubbleSort;
--- merge.js
+++ merge.js
@@ -1,5 +1,40 @@
-function merge() {}
+"use strict";
+
+/**accepts two sorted arrays and
+ * returns a new array with values
+ * from both arrays sorted. */
+
+function merge(nums1, nums2) {
+  //two pointers, one for each array
+
+  const len1 = nums1.length;
+  const len2 = nums2.length;
+  let i = 0;
+  let j = 0;
+  let output = []
+
+  while (i < len1 && j < len2) {
+    if (nums1[i] <= nums2[j]) {
+      output.push(nums1[i]);
+      i++
+    } else {
+      output.push(nums2[j]);
+      j++
+    }
+  }
+  while (j < len2) {
+    output.push(nums2[j]);
+      j++
+  }
+  while (i < len1) {
+    output.push(nums1[i]);
+      i++
+  }
+
+  return output
+}
+
 
 function mergeSort() {}
 
-module.exports = { merge, mergeSort};
+module.exports = { merge, mergeSort };
