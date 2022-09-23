3051c9558c07a9476e24819c6d4383ae9c126a0a
Andrew Jensen <andrewkjensen@gmail.com>
2022-09-23 13:32:29 -0700

    finish mergeSort

--- merge.js
+++ merge.js
@@ -5,11 +5,9 @@
  * from both arrays sorted. */
 
 function merge(nums1, nums2) {
-  //two pointers, one for each array
-  console.log("num1", nums1)
-  console.log("num2", nums2)
-  if(!nums1) return nums2
-  if(!nums2) return nums1
+
+  if (!nums1) return nums2;
+  if (!nums2) return nums1;
   const len1 = nums1.length;
   const len2 = nums2.length;
   let i = 0;
@@ -39,32 +37,12 @@ function merge(nums1, nums2) {
 
 /** Given an array, sort the values in the array */
 function mergeSort(nums) {
-
-  function split(nums) {
-    if (nums.length > 1) {
-      const arr1 = nums.slice(0, Math.floor(nums.length / 2));
-      const arr2 = nums.slice(Math.floor(nums.length / 2));
-      console.log("arr1", arr1)
-      console.log("arr2", arr2)
-      return merge(split(arr1), split(arr2));
-    }
-    // if (arr1.length > 1) {
-    //   return merge(split(arr1))
-    // } else if (arr2.length > 1) {
-    //   return merge(split(arr2))
-    // } else {
-    //   return merge(arr1, arr2)
-    // }
-
-
-  }
-
   if (nums.length > 1) {
-    return split(nums);
-  } else {
-    return nums;
+    const arr1 = mergeSort(nums.slice(0, Math.floor(nums.length / 2)));
+    const arr2 = mergeSort(nums.slice(Math.floor(nums.length / 2)));
+    return merge(arr1, arr2);
   }
-
+  return nums;
 }
 
 module.exports = { merge, mergeSort };
